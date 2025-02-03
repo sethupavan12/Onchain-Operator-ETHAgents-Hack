803bf60479493807b4d9363382aa4f885e2d9b0e
Kiearn Williams <kieranwilliams@vindico.net>
2025-02-03 00:34:34 +0000

    cleanup

--- chatbot.py
+++ chatbot.py
@@ -16,8 +16,10 @@ load_dotenv()
 
 # Import tools
 from tools import (
-    # crypto compare
+    # create an ERC-20 token
     deploy_multi_token, DeployMultiTokenInput, DEPLOY_MULTITOKEN_PROMPT,
+
+    # crypto compare
     fetch_news_tool, FetchNewsInput, FETCH_NEWS_PROMPT,
     fetch_price, FetchPriceInput, FETCH_PRICE_PROMPT,
     fetch_trading_signals, FetchTradingSignalsInput, FETCH_TRADING_SIGNALS_PROMPT,
@@ -29,20 +31,16 @@ from tools import (
     # Core wallet functions
     fetch_wallet_history, WalletHistoryInput, WALLET_HISTORY_PROMPT,
     fetch_wallet_balance, WalletBalanceInput, WALLET_BALANCE_PROMPT,
-    
     # NFT related functions
     fetch_nft_transfers, NFTTransfersInput, NFT_TRANSFERS_PROMPT,
     fetch_wallet_nft_trades, WalletNFTTradesInput, WALLET_NFT_TRADES_PROMPT,
-    
     # Token related functions
     fetch_token_transfers, TokenTransfersInput, TOKEN_TRANSFERS_PROMPT,
     fetch_wallet_tokens, WalletTokensInput, WALLET_TOKENS_PROMPT,
     fetch_token_price, TokenPriceInput, TOKEN_PRICE_PROMPT,
     fetch_batch_token_prices, BatchTokenPriceInput, BATCH_TOKEN_PRICES_PROMPT,
-    
     # DeFi related functions
     fetch_defi_positions, DeFiPositionsInput, DEFI_POSITIONS_PROMPT,
-    
     # Trading data functions
     fetch_pair_ohlcv, PairOHLCVInput, PAIR_OHLCV_PROMPT,
 
